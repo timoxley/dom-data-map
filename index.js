dd01569c86b5531bd446ec9a62bf20951095425b
Tim Oxley <secoif@gmail.com>
2013-03-31 13:34:49 -0400

    Add experiemental data inheritance

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 "use strict"
 
 var elData = require('data')
+var matches = require('matches-selector');
+
 var slice = Array.prototype.slice
 
 /**
@@ -15,8 +17,7 @@ var slice = Array.prototype.slice
  * @api public
  */
 
-module.exports = function setDomDataMapContext(context, el) {
-
+module.exports = function DomDataMapContext(context, el) {
 	/**
 	 * Calls an iterator `fn` on each Element matching
 	 * `selector`, passing in current element and its
@@ -30,28 +31,64 @@ module.exports = function setDomDataMapContext(context, el) {
 	 * @api public
 	 */
 
-	return function domDataMap(selector, fn) {
+	function domDataMap(selector, fn) {
 		var els = selector
 		if (els instanceof NodeList) els = slice.call(selector)
-		if (typeof selector === 'string') els = module.exports.query(selector, el)
+		if (typeof selector === 'string') els = query(selector, self.el)
 		els = els || []
 		els.forEach(function(el) {
 			var data = elData(el).get()
-			var returnedData = fn.call(context, el, data)
+      setParentData(el)
+			var returnedData = fn.call(data.context || self.context, el, data)
 			if (returnedData) elData(el).set(returnedData)
 		})
 	}
+  domDataMap.context = context
+  domDataMap.el = el
+  var self = domDataMap
+  return domDataMap
+}
+
+module.exports.setParentData = setParentData
+function setParentData(el, data) {
+  console.log('set parent data', el, data)
+  if (!el || el.nodeType === Node.DOCUMENT_NODE) return data
+  // BAD COUPLING TO yields/data implementation
+  // this checks if the element has associated data
+  if (el.__uniq) {
+    var foundData = elData(el).get()
+    if (!data) {
+      data = {}
+    }
+    data.__proto__ = foundData
+
+    return setParentData(el.parentNode, foundData)
+  }
+  return setParentData(el.parentNode, data)
 }
 
 /**
  * Override this for custom element selection.
- * @return {Array} Array of Elements in document matching selector
+ * @param {String} selector CSS Selector
+ * @param {Element} el Scope query to children of this element
+ * @return {Array} Array of Elements in el matching selector
  * @api public
  */
 
-module.exports.query = function query(selector, el) {
-  if (!el) el = document
-  if (typeof el === 'string') el = module.exports.query(el, document)
+module.exports.query = function(selector, el) {
+  var matchedEls = slice.call(el.querySelectorAll(selector))
+  if (matches(el, selector)) matchedEls.unshift(el)
+  return matchedEls
+}
+
+/**
+ * Argument handling for module.exports.query
+ * @return {Array} Array of Elements in document matching selector
+ * @api private
+ */
+function query(selector, el) {
+  if (!el) el = document.body
+  if (typeof el === 'string') el = query(el, document.body)
   if (typeof el.length !== 'undefined') el = el[0]
-	return slice.call(el.querySelectorAll(selector))
+	return module.exports.query(selector, el)
 }
